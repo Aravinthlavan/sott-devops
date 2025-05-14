b96fb889602a0405abff2a89584a9d2e81415400
BharathKT <90966025+BharathKT@users.noreply.github.com>
2025-05-14 22:44:54 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name = "Devops-RG"
-#     storage_account_name = "devopsstatefilessa"
-#     container_name = "tf-state"
-#     key = "dev.terraform.tfstate"
-#   }
-# }
-terraform {
- backend "local" {
- path = "C:/Users/Rahul/OneDrive/documents/devops/terraform/dev.terraform.tfstate"
+ terraform {
+   backend "azurerm" {
+     resource_group_name = "Dev-RG"
+     storage_account_name = "ttfstatesa"
+     container_name = "tfstate"
+     key = "terraform.tfstate"
+   }
  }
-}
+#terraform {
+# backend "local" {
+# path = "C:/Users/Rahul/OneDrive/documents/devops/terraform/dev.terraform.tfstate"
+# }
+#}
